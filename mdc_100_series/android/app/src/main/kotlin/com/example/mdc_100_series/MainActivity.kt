000b13596a626cfa0cbcf00bf5dad3e6fe910d3e
Wilder Zapata Miranda <wzeta21@gmail.com>
2019-12-27 17:29:00 -0400

    mdc basic

---
+++ mdc_100_series/android/app/src/main/kotlin/com/example/mdc_100_series/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.mdc_100_series
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
+}
