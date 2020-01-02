d0b70e6db40fa93cd7586be1ed8fca17d2496b3f
Wilder Zapata Miranda <wzeta21@gmail.com>
2020-01-02 12:16:27 -0400

    mdc-103 codelab --- chage the layout

--- mdc_100_series/lib/home.dart
+++ mdc_100_series/lib/home.dart
@@ -2,95 +2,92 @@ import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import 'model/products_repository.dart';
 import 'model/product.dart';
+import 'supplemental/asymmetric_view.dart';
 
 class HomePage extends StatelessWidget {
-  List<Card> _buildGridCards(BuildContext context) {
-    List<Product> products = ProductsRepository.loadProducts(Category.all);
-    if (products == null || products.isEmpty) {
-      return const <Card>[];
-    }
+  // List<Card> _buildGridCards(BuildContext context) {
+  //   List<Product> products = ProductsRepository.loadProducts(Category.all);
+  //   if (products == null || products.isEmpty) {
+  //     return const <Card>[];
+  //   }
 
-    final ThemeData theme = Theme.of(context);
-    final NumberFormat formatter = NumberFormat.simpleCurrency(
-        locale: Localizations.localeOf(context).toString());
+  //   final ThemeData theme = Theme.of(context);
+  //   final NumberFormat formatter = NumberFormat.simpleCurrency(
+  //       locale: Localizations.localeOf(context).toString());
 
-    return products.map((product) {
-      return Card(
-        clipBehavior: Clip.antiAlias,
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: <Widget>[
-            AspectRatio(
-              aspectRatio: 18 / 11,
-              child: Image.asset(product.assetName,
-                  package: product.assetPackage, fit: BoxFit.fitWidth),
-            ),
-            Expanded(
-              child: Padding(
-                padding: EdgeInsets.fromLTRB(16.0, 12.0, 16.0, 8.0),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: <Widget>[
-                    Text(
-                      product == null ? '' : product.name,
-                      style: theme.textTheme.button,
-                      softWrap: false,
-                      overflow: TextOverflow.ellipsis,
-                      maxLines: 1,
-                    ),
-                    SizedBox(
-                      height: 4.0,
-                    ),
-                    Text(
-                      product == null ? '' : formatter.format(product.price),
-                      style: theme.textTheme.caption,
-                    ),
-                  ],
-                ),
-              ),
-            )
-          ],
-        ),
-        elevation: 8.0,
-      );
-    }).toList();
-  }
+  //   return products.map((product) {
+  //     return Card(
+  //       clipBehavior: Clip.antiAlias,
+  //       child: Column(
+  //         crossAxisAlignment: CrossAxisAlignment.center,
+  //         children: <Widget>[
+  //           AspectRatio(
+  //             aspectRatio: 18 / 11,
+  //             child: Image.asset(product.assetName,
+  //                 package: product.assetPackage, fit: BoxFit.fitWidth),
+  //           ),
+  //           Expanded(
+  //             child: Padding(
+  //               padding: EdgeInsets.fromLTRB(16.0, 12.0, 16.0, 8.0),
+  //               child: Column(
+  //                 mainAxisAlignment: MainAxisAlignment.end,
+  //                 crossAxisAlignment: CrossAxisAlignment.center,
+  //                 children: <Widget>[
+  //                   Text(
+  //                     product == null ? '' : product.name,
+  //                     style: theme.textTheme.button,
+  //                     softWrap: false,
+  //                     overflow: TextOverflow.ellipsis,
+  //                     maxLines: 1,
+  //                   ),
+  //                   SizedBox(
+  //                     height: 4.0,
+  //                   ),
+  //                   Text(
+  //                     product == null ? '' : formatter.format(product.price),
+  //                     style: theme.textTheme.caption,
+  //                   ),
+  //                 ],
+  //               ),
+  //             ),
+  //           )
+  //         ],
+  //       ),
+  //       elevation: 8.0,
+  //     );
+  //   }).toList();
+  // }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Center(
-          child: Text('SHRINE'),
+        brightness: Brightness.light,
+        leading: IconButton(
+          icon: Icon(Icons.menu),
+          onPressed: () {
+            print('menu button');
+          },
         ),
+        title: Text('SHRINE'),
         actions: <Widget>[
           IconButton(
-            icon: Icon(Icons.search, semanticLabel: 'search'),
+            icon: Icon(Icons.search),
             onPressed: () {
-              print('Search botton');
+              print('Search button');
             },
           ),
           IconButton(
-            icon: Icon(Icons.tune, semanticLabel: 'filter'),
+            icon: Icon(Icons.tune),
             onPressed: () {
               print('Filter button');
             },
           )
         ],
-        leading: IconButton(
-          icon: Icon(Icons.menu, semanticLabel: 'menu'),
-          onPressed: () {
-            print('Menu button');
-          },
-        ),
       ),
-      body: GridView.count(
-          crossAxisCount: 2,
-          padding: EdgeInsets.all(16.0),
-          childAspectRatio: 8.0 / 9.0,
-          children: _buildGridCards(context)),
-      resizeToAvoidBottomInset: false,
+      body: AsymmetricView(
+        products: ProductsRepository.loadProducts(Category.all),
+      ),
     );
   }
 }
