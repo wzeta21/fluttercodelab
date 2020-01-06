9897e97926a3ff60a47ef891bf7a3fdddf627544
Wilder Zapata Miranda <wzeta21@gmail.com>
2020-01-06 09:52:09 -0400

    mdc-104 add a branded icon

--- mdc_100_series/lib/backdrop.dart
+++ mdc_100_series/lib/backdrop.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:mdc_100_series/login.dart';
 import 'package:meta/meta.dart';
 import 'model/product.dart';
+import 'login.dart';
 
 const double _kFlingVelocity = 2.0;
 
@@ -74,30 +76,42 @@ class _BackdropState extends State<Backdrop>
       elevation: 0.0,
       titleSpacing: 0.0,
       // leading: Icon(Icons.menu),
-      leading: IconButton(
-        icon: Icon(Icons.menu),
-        onPressed: _toggleBackdropLayerVisibility,
+      // leading: IconButton(
+      //   icon: Icon(Icons.menu),
+      //   onPressed: _toggleBackdropLayerVisibility,
+      // ),
+      title: _BackdropTitle(
+        listenable: _controller.view,
+        onPress: _toggleBackdropLayerVisibility,
+        frontTitle: widget.frontTitle,
+        backTitle: widget.backTitle,
       ),
-      title: Text('SHRINE'),
       actions: <Widget>[
         IconButton(
           icon: Icon(
             Icons.search,
-            semanticLabel: 'search',
+            semanticLabel: 'login',
           ),
           onPressed: () {
-            print('Buscando');
+            Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (BuildContext context) => LoginPage(),
+                ));
           },
         ),
         IconButton(
           icon: Icon(
             Icons.tune,
-            semanticLabel: 'Filtrando',
+            semanticLabel: 'login',
           ),
           onPressed: () {
-            print('Filtering');
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (BuildContext context) => LoginPage()),
+            );
           },
-        )
+        ),
       ],
     );
 
@@ -178,6 +192,87 @@ class _FrontLayer extends StatelessWidget {
           ),
           Expanded(
             child: child,
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class _BackdropTitle extends AnimatedWidget {
+  final Function onPress;
+  final Widget frontTitle;
+  final Widget backTitle;
+
+  const _BackdropTitle({
+    Key key,
+    Listenable listenable,
+    this.onPress,
+    @required this.frontTitle,
+    @required this.backTitle,
+  })  : assert(frontTitle != null),
+        assert(backTitle != null),
+        super(key: key, listenable: listenable);
+
+  @override
+  Widget build(BuildContext context) {
+    final Animation<double> animation = this.listenable;
+
+    return DefaultTextStyle(
+      style: Theme.of(context).primaryTextTheme.title,
+      softWrap: false,
+      overflow: TextOverflow.ellipsis,
+      child: Row(
+        children: <Widget>[
+          SizedBox(
+            width: 72.0,
+            child: IconButton(
+              padding: EdgeInsets.only(right: 8.0),
+              onPressed: this.onPress,
+              icon: Stack(
+                children: <Widget>[
+                  Opacity(
+                    opacity: animation.value,
+                    child: ImageIcon(AssetImage('assets/slanted_menu.png')),
+                  ),
+                  FractionalTranslation(
+                    translation: Tween<Offset>(
+                      begin: Offset.zero,
+                      end: Offset(1.0, 0.0),
+                    ).evaluate(animation),
+                    child: ImageIcon(AssetImage('assets/diamond.png')),
+                  )
+                ],
+              ),
+            ),
+          ),
+          Stack(
+            children: <Widget>[
+              Opacity(
+                opacity: CurvedAnimation(
+                  parent: ReverseAnimation(animation),
+                  curve: Interval(0.5, 1.0),
+                ).value,
+                child: FractionalTranslation(
+                  translation: Tween<Offset>(
+                    begin: Offset.zero,
+                    end: Offset(0.5, 0.0),
+                  ).evaluate(animation),
+                  child: backTitle,
+                ),
+              ),
+              Opacity(
+                opacity: CurvedAnimation(
+                        parent: animation, curve: Interval(0.5, 1.0))
+                    .value,
+                child: FractionalTranslation(
+                  translation:
+                      Tween<Offset>(begin: Offset(-0.25, 0.0), end: Offset.zero)
+                          .evaluate(animation),
+                  child: frontTitle,
+                ),
+              )
+            ],
           )
         ],
       ),
